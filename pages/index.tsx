d17db97124a81b416dacac8979e525d9d1f1e578
Rujal Acharya <076bei029.rujal@pcampus.edu.np>
2022-06-18 13:58:16 +0545

    Add layout for login page

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,14 +1,36 @@
-import { Stack, Heading } from '@chakra-ui/react';
+import { Stack, Heading, Flex, Input, Text, Button } from '@chakra-ui/react';
 import type { NextPage } from 'next';
+import Head from 'next/head';
 
 const Home: NextPage = () => (
-  <Stack spacing={6}>
-    <Heading as="h2" size="xl">
-      Never gonna give you up.
-      Never gonna let you down.
-      Never gonna run around and disheart you.
-    </Heading>
-  </Stack>
+  
+  <Flex height={"100vh"} alignItems={"center"} background={"gray.200"} justifyContent={"center"}>
+    <Flex direction={"column"} background={"white"} width={"640px"} height={"536px"} alignItems={"center"} justifyContent={"center"} p={12} rounded={6}>
+      <Heading color={"green.500"} width={"161px"} height={"43px"} marginBottom={"17px"}>this.form</Heading>
+      <Heading color={"black"} width={"295px"} height={"36px"} fontSize={"30px"} marginBottom={"22px"}>Create new account</Heading>
+      <Flex direction={"column"} width={"400px"} alignItems={"center"} p={"2px"} marginBottom={"28px"}>
+        <div>
+          <Text align={"left"}>Name</Text>
+          <Input width={"400px"} placeholder="John Doe"></Input>
+        </div>
+      </Flex>
+      <Flex direction={"column"} width={"400px"} alignItems={"center"} p={"2px"} marginBottom={"28px"}>
+        <div>
+          <Text align={"left"}>Email</Text>
+          <Input width={"400px"} placeholder="johndoe@example.com"></Input>
+        </div>
+        </Flex>
+      <Flex direction={"column"} width={"400px"} alignItems={"center"} p={"2px"} marginBottom={"28px"}>
+        <div>
+          <Text align={"left"}>Password</Text>
+          <Input width={"400px"} placeholder="********"></Input>
+        </div>
+      </Flex>
+      <Button  p={4} width={"400px"} height={"40px"} backgroundColor={"green.500"} color={"white"}>Create</Button>
+    </Flex>
+    
+  </Flex>
+  // );
 );
 
 export default Home;
